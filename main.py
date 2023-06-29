b99e2e52b3942e88eaccccc096fcfd871999e9f7
Leonard Mercedes <100613411+LMercedes03@users.noreply.github.com>
2023-06-29 00:21:44 -0400

    Complete ex1

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-
 # Update with the exercise you are trying to test
-from src import test
+from src import test, ex1
 
 
 def main():
+    # Test
     test.hello()
+
+    # Ex 1
+    people_list = [
+        {'name': 'alice', 'age': 20, 'weight': 160, 'sex': 'male', 'id': 1},
+        {'name': 'bob', 'age': 10, 'weight': 130, 'sex': 'male', 'id': 2},
+        {'name': 'charlie', 'age': 15, 'weight': 120, 'sex': 'female', 'id': 3},
+    ]
+    result = ex1.sort_people(people_list, 'weight', 'desc')
+    print(result)
 
 
 if __name__ == '__main__':
